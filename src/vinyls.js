7100477b61822626e863aea47828b9a5135bd1c6
AxelCHCR <axelchaves.r@hotmail.com>
2024-07-29 16:03:29 -0600

    fix: artist lowercase in vinyls.js

--- src/vinyls.js
+++ src/vinyls.js
@@ -2,7 +2,7 @@ const vinyls = [
   {
     "id": 1,
     "title": "After Hours",
-    "Artist": "The Weeknd",
+    "artist": "The Weeknd",
     "price": 45,
     "coverImage": "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Weeknd_-_After_Hours.png"
 
@@ -10,21 +10,21 @@ const vinyls = [
   {
     "id": 2,
     "title": "Kiss Land",
-    "Artist": "The Weeknd",
+    "artist": "The Weeknd",
     "price": 50,
     "coverImage": "https://upload.wikimedia.org/wikipedia/en/e/ed/The_Weeknd_-_Kiss_Land.png"
   },
   {
     "id": 3,
     "title": "Beauty Behind The Madness",
-    "Artist": "The Weeknd",
+    "artist": "The Weeknd",
     "price": 45,
     "coverImage": "https://upload.wikimedia.org/wikipedia/en/b/bd/The_Weeknd_-_Beauty_Behind_the_Madness.png"
   },
   {
     "id": 4,
     "title": "Trilogy",
-    "Artist": "The Weeknd",
+    "artist": "The Weeknd",
     "price": 45,
     "coverImage": "https://upload.wikimedia.org/wikipedia/en/9/96/The_Weeknd_-_Trilogy.png"
   }
